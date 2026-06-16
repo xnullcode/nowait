ad065c2d49927227b275e7856ab4f53d09fe5994
Adil Ali <adilalicm@gmail.com>
2026-06-16 14:49:11 +0530

    Create Spring Boot project with configured pom.xml

---
+++ backend/src/test/java/com/xnullcode/nowait/NowaitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xnullcode.nowait;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NowaitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
