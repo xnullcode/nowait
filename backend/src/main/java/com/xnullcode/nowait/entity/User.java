6aaba65252793d76425edab6654d24379f7b9bc8
Adil Ali <adilalicm@gmail.com>
2026-06-17 14:55:24 +0530

    Add user entity for cafe accounts

---
+++ backend/src/main/java/com/xnullcode/nowait/entity/User.java
@@ -0,0 +1,40 @@
+package com.xnullcode.nowait.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String cafeName;
+
+    public User() {}
+
+    public User(String username, String password, String cafeName) {
+        this.username = username;
+        this.password = password;
+        this.cafeName = cafeName;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+    
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getCafeName() { return cafeName; }
+    public void setCafeName(String cafeName) { this.cafeName = cafeName; }
+}
