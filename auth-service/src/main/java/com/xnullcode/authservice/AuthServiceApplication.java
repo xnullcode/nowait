af208710bbaa0b418e5423a619c419a5e4607616
Adil Ali <adilalicm@gmail.com>
2026-06-25 15:49:54 +0530

    feat(auth): add application entry point

---
+++ auth-service/src/main/java/com/xnullcode/authservice/AuthServiceApplication.java
@@ -0,0 +1,16 @@
+package com.xnullcode.authservice;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceApplication.class, args);
+	}
+
+	@org.springframework.context.annotation.Bean
+	public org.springframework.web.client.RestTemplate restTemplate() {
+		return new org.springframework.web.client.RestTemplate();
+	}
+}
