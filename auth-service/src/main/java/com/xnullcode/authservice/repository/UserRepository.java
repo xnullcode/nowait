14080bd99e07001b6783ce319d324d98fb78b6ea
Adil Ali <adilalicm@gmail.com>
2026-06-25 15:56:17 +0530

    feat(auth): create JPA repository for User data access

---
+++ auth-service/src/main/java/com/xnullcode/authservice/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.xnullcode.authservice.repository;
+
+import com.xnullcode.authservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
