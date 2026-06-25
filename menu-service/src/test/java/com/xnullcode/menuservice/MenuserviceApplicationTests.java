586fe71c5a076be2f7dd97a6e119c83a570580b9
Adil Ali <adilalicm@gmail.com>
2026-06-25 16:20:31 +0530

    feat(menu): add application entry point

---
+++ menu-service/src/test/java/com/xnullcode/menuservice/MenuserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xnullcode.menuservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
